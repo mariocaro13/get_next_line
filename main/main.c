ebb0198093f3bc0a97968279c13d892f99b4922f
Mario Caro Romero <mariocaro2002@gmail.com>
2024-05-10 22:06:41 +0200

    Main test how to save buffer in node->content

--- main/main.c
+++ main/main.c
@@ -4,15 +4,36 @@
 
 # include "../get_next_line.h"
 
+
+
 int	main(void)
 {
 	int	fd;
-	char	*content = "";
+       	static char	*buffer = NULL;
+	struct s_list	*node;
 	
-	fd = open("text.txt", O_RDONLY);
-	content = get_next_line(fd);
-	printf("%s", content);
+	char	*str =  "Pato";
+	printf("Str default: %s\n", str);
+
+	fd = open("short_text.txt", O_RDONLY);
+	if (fd > 0)
+	{
+		buffer = malloc(BUFFER_SIZE * sizeof(char));
+		if (read(fd, buffer, BUFFER_SIZE) > 0)
+		{
+			//	Metodo para guardar una str en Node->content
+			node = malloc(sizeof(t_list));
+			if (node)
+			{
+				node->content = buffer;
+				node->content[BUFFER_SIZE + 1] = '\0';
+				node->next = NULL;
+				printf("Node->content: %s\n", node->content);
+			}
+		}	
+		free(buffer);
+		free(node);
+	}
 	close(fd);
-	free(content);
 	return (0);
 }
